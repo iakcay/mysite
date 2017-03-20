ee6c97980615c319f9f143a382faec497c886369
İbrahim Akçay <ibrahim.akcay@emlakjet.com>
2017-03-20 22:32:00 +0300

    123

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,1 +1,11 @@
-import 'wp.pp'
+
+class { 'wordpress':
+  install_dir => '/var/www/wordpress',
+  db_name     => 'wordpress',
+  db_host     => 'localhost',
+  db_user     => 'wordpress',
+  db_password => 'insecure password',
+}
+
+    
+}
